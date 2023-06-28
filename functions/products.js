7d541c7163f9121e7565201c62b048863f8bc499
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-28 13:03:13 +0530

    fetching products data from airtable basic setup

--- functions/products.js
+++ functions/products.js
@@ -1,8 +1,28 @@
 // domain/.netlify/functions/products
 
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE);
+
 exports.handler = async function (event, context, cb) {
-  return {
-    statusCode: 200,
-    body: 'Products Route',
-  };
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+
+    console.log('##########');
+    console.log(response);
+    console.log('##########');
+
+    return {
+      statusCode: 200,
+      body: 'Products Route',
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'There was an error',
+    };
+  }
 };
