e2a8453f74923762f1c10212ede475053889d855
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-28 13:21:45 +0530

    refactoring the data from airtable to match the frontend data

--- functions/products.js
+++ functions/products.js
@@ -10,13 +10,45 @@ exports.handler = async function (event, context, cb) {
   try {
     const response = await airtable.list({ maxRecords: 200 });
 
+    const products = response.records.map((product) => {
+      const { id, fields } = product;
+
+      //   console.log('********************');
+      //   console.log(fields);
+      //   console.log('********************');
+      const {
+        name,
+        price,
+        featured,
+        company,
+        description,
+        category,
+        shipping,
+        images,
+        colors,
+      } = fields;
+      const { url } = images[0];
+      return {
+        id,
+        name,
+        price,
+        image: url,
+        featured,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+      };
+    });
+
     console.log('##########');
-    console.log(response);
+    console.log(products);
     console.log('##########');
 
     return {
       statusCode: 200,
-      body: 'Products Route',
+      body: JSON.stringify(products),
     };
   } catch (error) {
     console.log(error);
