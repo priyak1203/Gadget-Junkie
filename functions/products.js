ee48bbfdfcb7efc9fa3add3656c5ad487a487650
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-28 13:26:53 +0530

    fetching all products from airtable

--- functions/products.js
+++ functions/products.js
@@ -6,16 +6,12 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .base(process.env.AIRTABLE_BASE)
   .table(process.env.AIRTABLE_TABLE);
 
-exports.handler = async function (event, context, cb) {
+exports.handler = async function () {
   try {
     const response = await airtable.list({ maxRecords: 200 });
 
     const products = response.records.map((product) => {
       const { id, fields } = product;
-
-      //   console.log('********************');
-      //   console.log(fields);
-      //   console.log('********************');
       const {
         name,
         price,
@@ -28,6 +24,7 @@ exports.handler = async function (event, context, cb) {
         colors,
       } = fields;
       const { url } = images[0];
+
       return {
         id,
         name,
@@ -41,10 +38,6 @@ exports.handler = async function (event, context, cb) {
         shipping,
       };
     });
-
-    console.log('##########');
-    console.log(products);
-    console.log('##########');
 
     return {
       statusCode: 200,
