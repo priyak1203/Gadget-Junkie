04fd674ee02c841398ce376dee600854c449bf70
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-28 12:28:36 +0530

    airtable serverless functions basic setup

--- functions/single-product.js
+++ functions/single-product.js
@@ -1,6 +1,6 @@
 // domain/.netlify/functions/single-product
 
-exports.handler = async function () {
+exports.handler = async function (event, context, cb) {
   return {
     statusCode: 200,
     body: 'Single Product Route',
